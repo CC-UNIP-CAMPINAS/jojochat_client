c5c5c167b9b8d86794af834c2eb81d84ff239b4b
Leonardo Petta <leonardocps9@gmail.com>
2020-03-12 09:31:18 -0300

    Implemento de conexão com servidor local

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,22 @@
 package app;
 
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+        try {
+            Socket cliente = new Socket("localhost", 12345);
+            ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
+            Date data_atual = (Date) entrada.readObject();
+            JOptionPane.showMessageDialog(null, "Data recebida do servidor:" + data_atual.toString());
+            entrada.close();
+            System.out.println("Conexão encerrada");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
