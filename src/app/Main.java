85e19f9f98399390a55aade25f9be80ae67db8b0
Leonardo Petta <leonardocps9@gmail.com>
2020-03-12 13:04:54 -0300

    Criação da viewLogin e do SplashScreen

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,22 +1,92 @@
 package app;
 
-import java.io.ObjectInputStream;
-import java.net.Socket;
-import java.util.Date;
+import java.io.IOException;
 
-import javax.swing.JOptionPane;
+import javafx.application.Application;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+public class Main extends Application {
+    private static Stage stageLogin = new Stage();
+    private static Scene login;
+
+    @Override
+    public void start(Stage arg0) throws Exception {
         try {
-            Socket cliente = new Socket("localhost", 12345);
-            ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-            Date data_atual = (Date) entrada.readObject();
-            JOptionPane.showMessageDialog(null, "Data recebida do servidor:" + data_atual.toString());
-            entrada.close();
-            System.out.println("Conexão encerrada");
-        } catch (Exception e) {
+            FXMLLoader splashLoader = new FXMLLoader(getClass().getResource("/gui/views/SplashScreen.fxml"));
+            Parent splashPane = splashLoader.load();
+
+            // Cria a Janela do Splash
+            // Define como transparente para que não apareça decoração de janela (maximizar,
+            // minimizar)
+            Stage splashStage = new Stage(StageStyle.TRANSPARENT);
+            final Scene scene = new Scene(splashPane);
+            scene.setFill(Color.TRANSPARENT); // Define que a cor do painel root seja transparente para que dê o efeito
+                                              // de sombra
+            splashStage.setScene(scene);
+
+            // Cria o serviço para rodar alguma tarefa em background enquanto o splash é
+            // mostrado (no caso somente um delay de 10s)
+            Service<Boolean> splashService = new Service<Boolean>() {
+
+                // Mostra o splash quando o serviço for iniciado
+                @Override
+                public void start() {
+                    super.start();
+                    splashStage.show(); // mostra a janela
+                }
+
+                // Simulação de uma tarefa pesada
+                @Override
+                protected Task<Boolean> createTask() {
+                    return new Task<Boolean>() {
+                        @Override
+                        protected Boolean call() throws Exception {
+                            Thread.sleep(5000);
+                            return true;
+                        }
+                    };
+                }
+
+                // Quando a tarefa for finalizada fecha o splash e mostra a tela principal
+                @Override
+                protected void succeeded() {
+                    splashStage.close(); // Fecha o splash
+                    try {
+                        Parent fxmlLogin = FXMLLoader.load(getClass().getResource("/gui/views/ViewLogin.fxml"));
+                        login = new Scene(fxmlLogin);
+                        stageLogin.setScene(login);
+                        stageLogin.show();
+                        
+                    } catch (Exception ex) {
+                    }
+                }
+            };
+
+            splashService.start();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        /*
+         * try { Socket cliente = new Socket("localhost", 12345); ObjectInputStream
+         * entrada = new ObjectInputStream(cliente.getInputStream()); Date data_atual =
+         * (Date) entrada.readObject(); JOptionPane.showMessageDialog(null,
+         * "Data recebida do servidor:" + data_atual.toString()); entrada.close();
+         * System.out.println("Conexão encerrada"); } catch (Exception e) {
+         * e.printStackTrace(); }
+         */
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
     }
 }
