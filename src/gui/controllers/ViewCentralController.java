db2757b3e61b789cdc10fda0cf8ecd684bdecc1f
Leonardo Petta do Nascimento <leonardops9@protonmail.com>
2020-03-15 19:28:47 -0300

    Realocação de imports

--- src/gui/controllers/ViewCentralController.java
+++ src/gui/controllers/ViewCentralController.java
@@ -10,7 +10,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Vector;
 
-import app.Main;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
