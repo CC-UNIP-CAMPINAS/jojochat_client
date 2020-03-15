6d47f8002e156d6f9522e84ff1554f00ef233ccd
Leonardo Petta do Nascimento <leonardops9@protonmail.com>
2020-03-15 20:03:20 -0300

    Implementação de clique para abrir chat

--- src/gui/controllers/ViewCentralController.java
+++ src/gui/controllers/ViewCentralController.java
@@ -12,13 +12,20 @@ import java.util.Vector;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.stage.Stage;
 import utils.Connection;
 
 public class ViewCentralController implements Initializable {
 	Socket conexao;
+	
+	private static Stage stageChat = new Stage();
+	private static Scene sceneLogin;
 
 	static Vector<String> usuariosAtivos = new Vector<String>();
 
@@ -29,7 +36,7 @@ public class ViewCentralController implements Initializable {
 	@FXML
 	private TreeView<String> tvUsuariosAtivos;
 
-	public void carregaTreeView() throws ClassNotFoundException, IOException, IllegalStateException {
+	public void carregaTreeView() throws IOException {
 		TreeItem<String> treeItemPrincipal = new TreeItem<String>("Usuários Ativos");
 		treeItemPrincipal.setExpanded(true);
 
@@ -42,15 +49,33 @@ public class ViewCentralController implements Initializable {
 			@Override
 			public void run() {
 				tvUsuariosAtivos.setRoot(treeItemPrincipal);
-
 			}
 		});
+	}
+	
+	public void selecionaChat() {
+		if (tvUsuariosAtivos.getSelectionModel().getSelectedItem() != null
+				&& tvUsuariosAtivos.getSelectionModel().getSelectedItem().isLeaf()) {	
+			Platform.runLater(new Runnable() {
+				@Override
+				public void run() {
+					try {
+		                Parent fxmlChat = FXMLLoader.load(getClass().getResource("/gui/views/ViewChat.fxml"));
+		                sceneLogin = new Scene(fxmlChat);
+		                stageChat.setScene(sceneLogin);
+		                stageChat.show();
+		            } catch (Exception ex) {
+		            	ex.printStackTrace();
+		            }
+				}
+			});	
+		}
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		try {
-			Connection conexao = new Connection("localhost", 12345);
+			Connection conexao = new Connection("jojochatserver.hopto.org", 12345);
 			ObjectOutputStream saida = new ObjectOutputStream(conexao.getConnection().getOutputStream());
 			ObjectInputStream entrada = new ObjectInputStream(conexao.getConnection().getInputStream());
 			System.out.println(entrada.toString());
@@ -78,19 +103,19 @@ public class ViewCentralController implements Initializable {
 			try {
 				while (true) {
 					Object recebido = entrada.readObject();
-					ViewCentralController.usuariosAtivos = (Vector<String>) recebido;
-					carregaTreeView();
+					if (recebido instanceof Vector<?>) {
+						ViewCentralController.usuariosAtivos = (Vector<String>) recebido;
+						carregaTreeView();
+					}
 				}
 			}
 
 			catch (EOFException | SocketException e) {
 				e.printStackTrace();
-			} catch (ClassNotFoundException ex) {
-				System.out.println("Tipo de objeto não esperado");
 			} catch (IOException ex) {
 				ex.printStackTrace();
-			}
-			finally {
+			} catch (ClassNotFoundException e) {
+			} finally {
 				try {
 					cliente.close();
 				} catch (IOException e) {
--- src/gui/controllers/ViewChatController.java
+++ src/gui/controllers/ViewChatController.java
@@ -1,13 +1,6 @@
 package gui.controllers;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
 import java.net.URL;
-import java.net.UnknownHostException;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -18,10 +11,6 @@ import javafx.scene.control.TextArea;
 
 public class ViewChatController implements Initializable {
 
-	Socket cliente = null;
-	ObjectOutputStream saida = null;
-	ObjectInputStream entrada = null;
-	ArrayList<String> usuariosAtivos = new ArrayList<>();
 
 	@FXML
 	public TextArea taEscritura;
@@ -38,37 +27,15 @@ public class ViewChatController implements Initializable {
 
 	@FXML
 	public void enviaMensagem() {
-		String mensagem = taEscritura.getText();
-		try {
-			saida.writeObject(LocalDate.now() + " - " + mensagem);
-			gravaMensagemTextArea(LocalDate.now() + " - " + mensagem);
-			taEscritura.clear();
-
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 
 	public void gravaMensagemTextArea(String mensagem) {
-		String historico = taMensagens.getText();
-		historico += mensagem + "\n\n";
-		taMensagens.setText(historico);
+		
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		try {
-			cliente = new Socket("localhost", 12345);
-			saida = new ObjectOutputStream(cliente.getOutputStream());
-			entrada = new ObjectInputStream(cliente.getInputStream());
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 	}
 }
